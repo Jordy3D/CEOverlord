1c7a6803fb7ddd419247c0915ddeac6a06318779
scazzman99 <scazzman99@gmail.com>
2018-11-21 10:44:32 +1100

    doing boss stuff

---
+++ Assets/Scripts/Enemy/BossScripts/BossEnemy.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossEnemy : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public Transform player;
+    public EnemyAttack[] attacks;
+    public EnemyAttack currentAttack;
+    public float health;
+    public float timer;
+    public float delay;
+    bool canAttack = true;
+    public bool canAct, activated;
+    public bool belowHalf;
+
+    // Use this for initialization
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (canAct && activated)
+        {
+            //run timer to see if we can attack
+            if (canAttack == false)
+            {
+                timer += Time.deltaTime;
+                if (timer >= delay)
+                {
+                    canAttack = true;
+                    timer = 0f;
+                }
+            }
+            else
+            {
+
+                int attackIndex = Mathf.RoundToInt(Random.Range(0, attacks.Length));
+                currentAttack = attacks[attackIndex];
+                canAttack = false;
+                delay = currentAttack.delay;
+                if (currentAttack.GetComponent<BossRadial>())
+                {
+                    BossRadial radialAttack = currentAttack.GetComponent<BossRadial>();
+                    radialAttack.CallMulti(5);
+                    canAct = false;
+                }
+
+            } 
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Enemy/BossScripts/BossRadial.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossRadial : EnemyAttack {
+
+    public float radius;
+    public int numProjectiles;
+    public BossEnemy boss;
+
+    private void Start()
+    {
+        boss = transform.parent.GetComponent<BossEnemy>();
+        
+        
+    }
+    public override void Attack()
+    {
+        //number of turns we need to make for radial attack
+        Vector3 startPoint = this.transform.position;
+        float angleStep = 360 / numProjectiles;
+        //Debug.Log(angleStep);
+        float currentAngle = 0f;
+
+        for (int i = 0; i < numProjectiles; i++)
+        {
+            Debug.Log(currentAngle);
+            float dirX = startPoint.x + Mathf.Sin((currentAngle * Mathf.PI) / 180) * radius;
+            float dirY = startPoint.y + Mathf.Cos((currentAngle * Mathf.PI) / 180) * radius;
+            Vector3 shotDir = new Vector3(dirX, dirY, 0f);
+            Vector3 shotMoveDir = (shotDir - startPoint).normalized;
+
+            GameObject clone = Instantiate(projectile, startPoint, Quaternion.identity);
+            EnemyProjectile newProjectile = clone.GetComponent<EnemyProjectile>();
+
+            newProjectile.Fire(new Vector3(shotMoveDir.x, 0, shotMoveDir.y));
+
+            currentAngle += angleStep;
+
+        }
+        Debug.Log("Reached nums - 1");
+    }
+
+    public void AttackMulti(float angleShift)
+    {
+        //number of turns we need to make for radial attack
+        Vector3 startPoint = this.transform.position;
+        float angleStep = 360 / numProjectiles;
+        //Debug.Log(angleStep);
+        float currentAngle = angleShift;
+
+        for (int i = 0; i < numProjectiles; i++)
+        {
+            Debug.Log(currentAngle);
+            float dirX = startPoint.x + Mathf.Sin((currentAngle * Mathf.PI) / 180) * radius;
+            float dirY = startPoint.y + Mathf.Cos((currentAngle * Mathf.PI) / 180) * radius;
+            Vector3 shotDir = new Vector3(dirX, dirY, 0f);
+            Vector3 shotMoveDir = (shotDir - startPoint).normalized;
+
+            GameObject clone = Instantiate(projectile, startPoint, Quaternion.identity);
+            EnemyProjectile newProjectile = clone.GetComponent<EnemyProjectile>();
+
+            newProjectile.Fire(new Vector3(shotMoveDir.x, 0, shotMoveDir.y));
+
+            currentAngle += angleStep;
+
+        }
+        Debug.Log("Reached nums - 1");
+    }
+
+    IEnumerator radialMulti(int repeats, float shift)
+    {
+        float angleShift = 0f;
+        for (int i = 0; i < repeats; i++)
+        {
+            AttackMulti(angleShift);
+            angleShift += shift;
+            yield return new WaitForSeconds(0.7f); 
+        }
+        boss.canAct = true;
+    }
+
+    public void CallMulti(int repeats)
+    {
+        
+        StartCoroutine(radialMulti(repeats, 15f));
+    }
+}
