7de173dcb32b4d09df710811f98bc8260fbe5f46
Jordy3D <fchetar@gmail.com>
2018-11-29 10:31:33 +1100

    Cool Changes and Attack Range is a thing

---
+++ Assets/PlayerRange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRange : MonoBehaviour
+{
+    public GameObject rangeTarget;
+    public float range;
+
+    PlayerStats stats;
+    // Use this for initialization
+    void Start()
+    {
+        stats = GetComponent<PlayerStats>();
+        range = stats.attackRange;
+
+        UpdateRange(range);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRange(range);
+    }
+
+    void UpdateRange(float range)
+    {
+        range = stats.attackRange;
+
+        rangeTarget.transform.localPosition = new Vector3(0, 0, range);
+    }
+}
